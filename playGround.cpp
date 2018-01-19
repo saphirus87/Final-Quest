fd4d7299ff22c600c829e0bdc7b7ff5bc94068d6
이호형 <saphirus87@gmail.com>
2018-01-19 15:40:26 +0900

    기본 프로젝트

--- playGround.cpp
+++ playGround.cpp
@@ -16,24 +16,6 @@ HRESULT playGround::init()
 {
 	gameNode::init(true);
 
-	IMAGEMANAGER->addImage("바탕", "우주.bmp", WINSIZEX, WINSIZEY, false, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("battle", "battle.bmp", 0, 0, 1536, 79, 16, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("battle2", "battle.bmp", 0, 0, 1536, 79, 16, 1, true, RGB(255, 0, 255));
-
-
-	IMAGEMANAGER->addImage("bullet", "bullet.bmp", 21, 21, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("missilePF", "missilePF.bmp", 0, 0, 576, 44, 16, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("버튼", "button.bmp", 0, 0, 122, 62, 1, 2, true, RGB(255, 0, 255));
-	
-	_select = new selectScene;
-	
-	SCENEMANAGER->addScene("셀렉트씬", _select);
-	SCENEMANAGER->addScene("스타씬", new starcraftScene);
-
-	SCENEMANAGER->changeScene("스타씬");
-
-
-	
 	return S_OK;
 }
 
@@ -58,10 +40,8 @@ void playGround::update(void)
 void playGround::render(void)
 {
 	PatBlt(getMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
-	//================== 이 위는 손대지 마시오 =========================
+	//================== 이 위는 손대지 마시오 ========================
 
-	IMAGEMANAGER->findImage("바탕")->render(getMemDC(),0,0);
-	
 	SCENEMANAGER->render();
 	
 	SetTextColor(getMemDC(), RGB(255, 255, 255));
