ff3f2a2c78ea77af85c167676beb5cdb127d4e18
이호형 <saphirus87@gmail.com>
2018-01-19 16:09:39 +0900

    기본 프로젝트 제발~~~제발~~

---
+++ enemyManager.h
@@ -0,0 +1,7 @@
+#pragma once
+class enemyManager
+{
+public:
+	enemyManager();
+	~enemyManager();
+};
---
+++ stageManager.h
@@ -0,0 +1,8 @@
+#pragma once
+class stageManager
+{
+public:
+	stageManager();
+	~stageManager();
+};
+
